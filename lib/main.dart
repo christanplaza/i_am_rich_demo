628b883319264961e4f4fa5b0b488b4d29df6f2e
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2022-01-03 23:47:14 +0800

    Adding Centered, format on save

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(home: Text("Hello World")));
+void main() => runApp(
+      const MaterialApp(
+        home: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
