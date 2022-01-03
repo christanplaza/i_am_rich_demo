5a284c6ed8a666224e0af46f878e9cc3f258138f
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2022-01-03 23:48:13 +0800

    Final Formatting for Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      const MaterialApp(
-        home: Center(
-          child: Text("Hello World"),
-        ),
+// The main function is the starting point for all our Flutter apps.
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
       ),
-    );
+    ),
+  );
+}
