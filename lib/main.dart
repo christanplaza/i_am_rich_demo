471829987613205609eb5bafce35cdb7c46a1bb7
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2022-01-03 23:56:31 +0800

    Adding Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 // The main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
       ),
     ),
   );
